6ce1a3485705149d8b28658df0b173b1b254067a
tlvlp <tllvllp@gmail.com>
2024-08-27 01:57:56 +0200

    File traversing to read all questions - wip

--- shellingo_cli/src/main.rs
+++ shellingo_cli/src/main.rs
@@ -1,14 +1,22 @@
 use std::{collections::HashSet, path::PathBuf};
 use clap::{arg, command, value_parser};
 use shellingo_core::question::Question;
+use walkdir::{DirEntry, WalkDir};
 
 const DEFAULT_PATH: &str = "questions";
 const CLUE_PENALTY: i32 = 10;
 const REVEAL_PENALTY:i32 = 5;
 
 fn main() {
-    // Get the questions' path
-    let path = command!()
+    let path = get_path_from_cli_args(); 
+    
+    println!("Loading questions from path: {}", path.display());
+    let questions = read_all_questions_from(path);
+
+}
+
+fn get_path_from_cli_args() -> PathBuf {
+    command!()
         .arg(arg!(<path> "Path to the questions. Either a file or a directory.")
            .required(false)
            .default_value(DEFAULT_PATH)
@@ -17,14 +25,26 @@ fn main() {
         .get_matches()
         .get_one::<PathBuf>("path")
         .expect("Required field!")
-        .to_owned();
-    let path_str = path.display();
-    println!("Loading questions from path: {}", path_str);
-    
-    let questions = get_all_questions_at_path(path);
-
+        .to_owned()
 }
 
-fn get_all_questions_at_path(path: PathBuf) -> HashSet<Question> {
+fn read_all_questions_from(path: PathBuf) -> HashSet<Question> {
+        WalkDir::new(path)
+            .into_iter()
+            .filter_map(Result::ok)
+            .filter(|entry| entry.path().is_file())
+            .flat_map(read_lines_in_file)
+            .map(parse_question_from_line)
+            .collect()
+}
+
+fn read_lines_in_file(entry: DirEntry) -> HashSet<String> {
+    todo!()
+            // For example, you can read the file here
+            // let content = fs::read_to_string(path).expect("Could not read file");
+            // println!("File content: {}", content);
+}
+
+fn parse_question_from_line(line: String) -> Question {
     todo!()
 }
--- shellingo_core/src/question.rs
+++ shellingo_core/src/question.rs
@@ -2,14 +2,29 @@ use std::{collections::HashSet, hash::Hash};
 
 #[derive(Debug)]
 pub struct Question {
+   pub location: String,
    pub question: String,
    pub solutions: HashSet<String>,
-   pub location: String,
 
    pub correct_count_round: i32,
    pub error_count_round: i32,
    pub correct_count_sum: i32,
    pub error_count_sum: i32,    
+}
+
+impl Question {
+   fn new(location: String, quesion: String, solutions: HashSet<String>) -> Question {
+      Question {
+         location: location, 
+         question: quesion,
+         solutions: solutions,
+
+         correct_count_round: 0,
+         error_count_round: 0,
+         correct_count_sum: 0,
+         error_count_sum: 0
+      }
+   }
 }
 
 impl PartialEq for Question {
